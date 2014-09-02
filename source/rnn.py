f0b88f766415ea5643ad7b1f956df04b85503f7e
HanZhao <zhaohan2009011312@gmail.com>
2014-09-02 09:23:11 +0800

    use fully BRNN

--- source/rnn.py
+++ source/rnn.py
@@ -124,8 +124,7 @@ class TBRNN(object):
 		self.h_start = theano.shared(value=np.zeros(fan_out, dtype=floatX), name='h_start', borrow=True)
 		self.h_end = theano.shared(value=np.zeros(fan_out, dtype=floatX), name='h_end', borrow=True)
 		# Save all the parameters
-		# self.params = [self.W, self.U, self.b, self.h_start, self.h_end]
-		self.params = []
+		self.params = [self.W, self.U, self.b, self.h_start, self.h_end]
 		# recurrent function used to compress a sequence of input vectors
 		# the first dimension should correspond to time
 		def step(x_t, h_tm1):
