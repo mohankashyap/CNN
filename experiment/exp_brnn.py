203c12461f2484e0dce6c37a57fcae1f3ab59aa4
HanZhao <zhaohan2009011312@gmail.com>
2014-09-04 11:29:34 +0800

    save temporary results

--- experiment/exp_brnn.py
+++ experiment/exp_brnn.py
@@ -103,8 +103,8 @@ class TestBRNN(unittest.TestCase):
 		self.test_size = test_size
 		self.word_embedding = word_embedding
 
-	# @unittest.skip('Wait a minute')
-	def testBRNNonSentiment(self):
+	@unittest.skip('Wait a minute')
+	def testTBRNNonSentiment(self):
 		# Set print precision
 		np.set_printoptions(threshold=np.nan)
 
@@ -198,68 +198,106 @@ class TestBRNN(unittest.TestCase):
 									   'training_backward' : training_backward_rep, 
 									   'test_forward' : test_forward_rep, 
 									   'test_backward' : test_backward_rep})
-		# Save BRNN
+		# Save TBRNN
 		TBRNN.save('sentiment.brnn.pkl', brnn)
 		pprint('Model successfully saved...')
 
-	@unittest.skip('skip')
-	def testSoftmaxWithLearned(self):
-		data = sio.loadmat('./BRNN-rep.mat')
-		train_data = np.hstack((data['training_forward'], data['training_backward']))
-		test_data = np.hstack((data['test_forward'], data['test_backward']))
-		pprint('Training matrix dimension: ')
-		pprint(train_data.shape)
-		pprint('Test matrix dimension: ')
-		pprint(test_data.shape)
-		input = T.matrix(name='input')
-		truth = T.ivector(name='label')
-		learn_rate = T.scalar(name='learning rate')
-		softmax = SoftmaxLayer(input, (100, 2))
-		cost = softmax.NLL_loss(truth)
-		params = softmax.params
-		gradparams = T.grad(cost, params)
-		updates = []
-		for param, gradparam in zip(params, gradparams):
-			updates.append((param, param-learn_rate*gradparam))
-		train = theano.function(inputs=[input, truth, learn_rate], outputs=cost, 
-								updates=updates)
-		nepoch = 20000
-		rate = 1e-1
-		start_time = time.time()
-		for i in xrange(nepoch):
-			tot_cost = train(train_data, self.senti_train_label, rate)
-			prediction = softmax.predict(train_data)
-			accuracy = np.sum(prediction == self.senti_train_label) / float(self.train_size)
-			pprint('Epoch %d, total error: %f, accuracy: %f' % (i, tot_cost, accuracy))
-		end_time = time.time()
-		pprint('Time used for training: %f' % (end_time-start_time))
-		# Testing
-		prediction = softmax.predict(test_data)
-		accuracy = np.sum(prediction == self.senti_test_label) / float(self.test_size)
-		pprint('Test accuracy: %f' % accuracy)
+	# @unittest.skip('Wait a minute')
+	def testBRNNonSentiment(self):
+		# Set print precision
+		np.set_printoptions(threshold=np.nan)
 
-	@unittest.skip('bug found!')
-	def testResult(self):
-		data = sio.loadmat('./BRNN-rep.mat')
-		train_data = np.hstack((data['training_forward'], data['training_backward']))
-		test_data = np.hstack((data['test_forward'], data['test_backward']))
-		pprint('Training matrix dimension: ')
-		pprint(train_data.shape)
-		pprint('Test matrix dimension: ')
-		pprint(test_data.shape)
+		config_filename = './sentiment_brnn.conf'
 		start_time = time.time()
-		brnn = TBRNN.load('sentiment.brnn.pkl')
-		W = brnn.softmax.W.get_value(borrow=True)
-		b = brnn.softmax.b.get_value(borrow=True)
+		configer = RNNConfiger(config_filename)
+		brnn = BRNN(configer, verbose=True)
 		end_time = time.time()
-		tmp_softmax = np.dot(train_data, W) + b
-		tmp_train_label = np.argmax(tmp_softmax, axis=1)
-		accuracy = np.sum(tmp_train_label == self.senti_train_label) / float(self.train_size)
-		pprint('Training accuracy: %f' % accuracy)
-		tmp_softmax = np.dot(test_data, W) + b
-		tmp_test_label = np.argmax(tmp_softmax, axis=1)
-		accuracy = np.sum(tmp_test_label == self.senti_test_label) / float(self.test_size)
-		pprint('Test accuracy: %f' % accuracy)
+		pprint('Time used to build BRNN: %f seconds.' % (end_time-start_time))
+		# Training
+		pprint('positive labels: %d' % np.sum(self.senti_train_label))
+		pprint('negative labels: %d' % (self.senti_train_label.shape[0]-np.sum(self.senti_train_label)))
+		start_time = time.time()
+		## AdaGrad learning algorithm instead of the stochastic gradient descent algorithm
+		history_grads = np.zeros(brnn.num_params)
+		n_epoch = 2000
+		learn_rate = 1
+		fudge_factor = 1e-6
+		for i in xrange(n_epoch):
+			tot_count = 0
+			tot_error = 0.0
+			conf_matrix = np.zeros((2, 2), dtype=np.int32)
+			tot_grads = np.zeros(brnn.num_params)
+			pprint('Total number of parameters in TBRNN: %d' % brnn.num_params)
+			for train_seq, train_label in zip(self.senti_train_set, self.senti_train_label):
+				cost, current_grads = brnn.compute_cost_and_gradient(train_seq, [train_label])
+				tot_grads += current_grads
+				tot_error += cost
+				# historical gradient accumulation
+				history_grads += current_grads ** 2
+				# predict current training label
+				prediction = brnn.predict(train_seq)[0]
+				tot_count += prediction == train_label
+				conf_matrix[train_label, prediction] += 1
+			# Batch updating 
+			tot_grads /= self.train_size
+			# Update historical gradient vector
+			adjusted_grads = tot_grads / (fudge_factor + np.sqrt(history_grads))
+			brnn.update_params(adjusted_grads, learn_rate)
+			# End of the core AdaGrad updating algorithm
+			accuracy = tot_count / float(self.train_size)
+			pprint('Epoch %d, total cost: %f, overall accuracy: %f' % (i, tot_error, accuracy))
+			pprint('Confusion matrix: ')
+			pprint(conf_matrix)
+			pprint('-' * 50)
+			if (i+1) % 100 == 0:
+				pprint('=' * 50)
+				pprint('Test at epoch: %d' % i)
+				# Testing
+				tot_count = 0
+				for test_seq, test_label in zip(self.senti_test_set, self.senti_test_label):
+					prediction = brnn.predict(test_seq)[0]
+					tot_count += test_label == prediction
+				pprint('Test accuracy: %f' % (tot_count / float(self.test_size)))
+				pprint('Percentage of positive in Test data: %f' % (np.sum(self.senti_test_label==1) / float(self.test_size)))
+				pprint('Percentage of negative in Test data: %f' % (np.sum(self.senti_test_label==0) / float(self.test_size)))
+				pprint('=' * 50)
+		end_time = time.time()
+		pprint('Time used for training: %f minutes.' % ((end_time-start_time)/60))
+		# Testing
+		tot_count = 0
+		for test_seq, test_label in zip(self.senti_test_set, self.senti_test_label):
+			prediction = brnn.predict(test_seq)[0]
+			tot_count += test_label == prediction
+		pprint('Test accuracy: %f' % (tot_count / float(self.test_size)))
+		pprint('Percentage of positive in Test data: %f' % (np.sum(self.senti_test_label==1) / float(self.test_size)))
+		pprint('Percentage of negative in Test data: %f' % (np.sum(self.senti_test_label==0) / float(self.test_size)))
+		# Re-testing on training set
+		tot_count = 0
+		for train_seq, train_label in zip(self.senti_train_set, self.senti_train_label):
+			prediction = brnn.predict(train_seq)[0]
+			tot_count += train_label == prediction
+		pprint('Training accuracy re-testing: %f' % (tot_count / float(self.train_size)))
+		# Show representation for training inputs and testing inputs
+		start_time = time.time()
+		training_forward_rep = np.zeros((self.train_size, configer.num_hidden))
+		test_forward_rep = np.zeros((self.test_size, configer.num_hidden))
+		training_backward_rep = np.zeros((self.train_size, configer.num_hidden))
+		test_backward_rep = np.zeros((self.test_size, configer.num_hidden))
+		for i, train_seq in enumerate(self.senti_train_set):
+			training_forward_rep[i, :] = brnn.show_forward(train_seq)
+			training_backward_rep[i, :] = brnn.show_backward(train_seq)
+		for i, test_seq in enumerate(self.senti_test_set):
+			test_forward_rep[i, :] = brnn.show_forward(test_seq)
+			test_backward_rep[i, :] = brnn.show_backward(test_seq)
+		end_time = time.time()
+		pprint('Time used to show forward and backward representation for training and test instances: %f seconds' % (end_time-start_time))
+		sio.savemat('./nBRNN-rep.mat', {'training_forward' : training_forward_rep, 
+									   'training_backward' : training_backward_rep, 
+									   'test_forward' : test_forward_rep, 
+									   'test_backward' : test_backward_rep})
+		# Save TBRNN
+		TBRNN.save('sentiment.nbrnn.pkl', brnn)
+		pprint('Model successfully saved...')
 
 
 if __name__ == '__main__':
--- source/rnn.py
+++ source/rnn.py
@@ -185,16 +185,8 @@ class TBRNN(object):
 		if configs.regularization:
 			self.cost += configs.lambda1 * self.L2_norm
 		# Compute gradient
-		# For the convenience of computing the gradient of each component
-		# self.params = [self.W, self.U, self.b, self.W_softmax, self.b_softmax]
 		self.gradtheta = T.grad(self.cost, self.theta)
-		# self.gradparams = T.grad(self.cost, self.params)
-		# self.updates = []
-		# for param, gradparam in zip(self.params, self.gradparams):
-		# 	self.updates.append((param, param-self.learn_rate*gradparam))
 		# Build objective function
-		# self.objective = theano.function(inputs=[self.input, self.truth, self.learn_rate], \
-		# 								 outputs=self.cost, updates=self.updates)
 		# Compute the gradients
 		self.compute_cost_and_gradient = theano.function(inputs=[self.input, self.truth], 
 												outputs=[self.cost, self.gradtheta])
@@ -232,3 +224,166 @@ class TBRNN(object):
 	def load(fname):
 		with file(fname, 'rb') as fin:
 			return cPickle.load(fin)
+
+
+class BRNN(object):
+	'''
+	Bidirectional RNN. This is just a trial for using 
+	BRNN as a tool for sentence modeling.
+
+	First trial on the task of sentiment analysis.
+	'''
+	def __init__(self, configs, verbose=True):
+		if verbose: pprint('Build Tied weights Bidirectional Recurrent Neural Network')
+		self.input = T.matrix(name='input')
+		self.truth = T.ivector(name='label')
+		self.learn_rate = T.scalar(name='learn rate')
+		# Configure Activation function
+		self.act = Activation(configs.activation)
+		# Build bidirectional RNN with tied weights
+		num_input, num_hidden, num_class = configs.num_input, configs.num_hidden, configs.num_class
+		# Stack all the variables together into a vector in order to apply the batch updating algorithm
+		# Since there are two directions for the RNN, all the weight matrix associated with RNN will be 
+		# duplicated
+		num_params = 2 * (num_input * num_hidden + \
+					 num_hidden * num_hidden + \
+					 num_hidden) + \
+					 2 * num_hidden * num_class + \
+					 num_class
+		self.num_params = num_params
+		self.theta = theano.shared(value=np.zeros(num_params, dtype=floatX), name='theta', borrow=True)
+		# Incremental index
+		param_idx = 0
+		# 1, Feed-forward matrix for forward direction: W_forward
+		self.W_forward = self.theta[param_idx: param_idx+num_input*num_hidden].reshape((num_input, num_hidden))
+		self.W_forward.name = 'W_forward_RNN'
+		W_forward_init = np.asarray(np.random.uniform(low=-np.sqrt(6.0/(num_input+num_hidden)),
+									  		  		  high=np.sqrt(6.0/(num_input+num_hidden)),
+									  		  		  size=(num_input, num_hidden)), dtype=floatX)
+		param_idx += num_input * num_hidden
+		# 1, Feed-forward matrix for backward direction: W_backward
+		self.W_backward = self.theta[param_idx: param_idx+num_input*num_hidden].reshape((num_input, num_hidden))
+		self.W_backward.name = 'W_backward_RNN'
+		W_backward_init = np.asarray(np.random.uniform(low=-np.sqrt(6.0/(num_input+num_hidden)),
+													   high=np.sqrt(6.0/(num_input+num_hidden)),
+													   size=(num_input, num_hidden)), dtype=floatX)
+		param_idx += num_input * num_hidden
+		# 2, Recurrent matrix for forward direction: U_forward
+		self.U_forward = self.theta[param_idx: param_idx+num_hidden*num_hidden].reshape((num_hidden, num_hidden))
+		self.U_forward.name = 'U_forward_RNN'
+		U_forward_init = np.asarray(np.random.uniform(low=-np.sqrt(6.0/(num_hidden+num_hidden)),
+													  high=np.sqrt(6.0/(num_hidden+num_hidden)),
+													  size=(num_hidden, num_hidden)), dtype=floatX)
+		param_idx += num_hidden * num_hidden
+		# 2, Recurrent matrix for backward direction: U_backward
+		self.U_backward = self.theta[param_idx: param_idx+num_hidden*num_hidden].reshape((num_hidden, num_hidden))
+		self.U_backward.name = 'U_backward_RNN'
+		U_backward_init = np.asarray(np.random.uniform(low=-np.sqrt(6.0/(num_hidden+num_hidden)),
+													   high=np.sqrt(6.0/(num_hidden+num_hidden)),
+													   size=(num_hidden, num_hidden)), dtype=floatX)
+		param_idx += num_hidden * num_hidden
+		# 3, Bias parameter for the hidden-layer forward direction RNN
+		self.b_forward = self.theta[param_idx: param_idx+num_hidden]
+		self.b_forward.name = 'b_forward_RNN'
+		b_forward_init = np.zeros(num_hidden, dtype=floatX)		
+		param_idx += num_hidden
+		# 3, Bias parameter for the hidden-layer backward direction RNN
+		self.b_backward = self.theta[pram_idx: param_idx+num_hidden]
+		self.b_backward.name = 'b_backward_RNN'
+		b_backward_init = np.zeros(num_hidden, dtype=floatX)
+		param_idx += num_hidden
+		# Weight matrix for softmax function
+		self.W_softmax = self.theta[param_idx: param_idx+2*num_hidden*num_class].reshape((2*num_hidden, num_class))
+		self.W_softmax.name = 'W_softmax'
+		W_softmax_init = np.asarray(np.random.uniform(low=-np.sqrt(6.0/(2*num_hidden+num_class)), 
+													  high=np.sqrt(6.0/(2*num_hidden+num_class)),
+													  size=(2*num_input, num_class)), dtype=floatX)
+		param_idx += 2*num_hidden*num_class
+		# Bias vector for softmax function
+		self.b_softmax = self.theta[param_idx: param_idx+num_class]
+		self.b_softmax.name = 'b_softmax'
+		b_softmax_init = np.zeros(num_class, dtype=floatX)
+		param_idx += num_class
+		# Set all the default parameters into theta
+		self.theta.set_value(np.concatenate([x.ravel() for x in 
+			(W_forward_init, W_backward_init, U_forward_init, U_backward_init, 
+			 b_forwrad_init, b_backward_init, W_softmax_init, b_softmax_init)]))
+		assert param_idx == num_params
+		# h[0], zero vector, treated as constants
+		self.h_start = theano.shared(value=np.zeros(num_hidden, dtype=floatX), name='h_start', borrow=True)
+		self.h_end = theano.shared(value=np.zeros(num_hidden, dtype=floatX), name='h_end', borrow=True)
+		# recurrent function used to compress a sequence of input vectors
+		# the first dimension should correspond to time
+		def forward_step(x_t, h_tm1):
+			h_t = self.act.activate(T.dot(x_t, self.W_forward) + \
+									T.dot(h_tm1, self.U_forward) + self.b_forward)
+			return h_t
+		def backward_step(x_t, h_tm1):
+			h_t = self.act.activate(T.dot(x_t, self.W_backward) + \
+									T.dot(h_tm1, self.U_backward) + self.b_backward)
+			return h_t
+		# Forward and backward representation over time
+		self.forward_h, _ = theano.scan(fn=forward_step, sequences=self.input, outputs_info=[self.h_start])
+		self.backward_h, _ = theano.scan(fn=backward_step, sequences=self.input, outputs_info=[self.h_end], go_backwards=True)
+		# Store the final value
+		self.h_start_star = self.forward_h[-1]
+		self.h_end_star = self.backward_h[-1]
+		# L1, L2 regularization
+		self.L1_norm = T.sum(T.abs_(self.W) + T.abs_(self.U) + T.abs_(self.W_softmax))
+		self.L2_norm = T.sum(self.W ** 2) + T.sum(self.U ** 2) + T.sum(self.W_softmax ** 2)
+		# Build function to show the learned representation for different sentences
+		self.show_forward = theano.function(inputs=[self.input], outputs=self.h_start_star)
+		self.show_backward = theano.function(inputs=[self.input], outputs=self.h_end_star)
+		##################################################################################
+		# Correlated BRNN
+		##################################################################################
+		# Concatenate these two vectors into one
+		self.h = T.concatenate([self.h_start_star, self.h_end_star], axis=0)
+		# Use concatenated vector as input to the Softmax/MLP classifier
+		self.output = T.nnet.softmax(T.dot(self.h, self.W_softmax) + self.b_softmax)		
+		self.pred = T.argmax(self.output, axis=1)
+		# Build cost function
+		self.cost = -T.mean(T.log(self.output)[T.arange(self.truth.shape[0]), self.truth])
+		if configs.regularization:
+			self.cost += configs.lambda1 * self.L2_norm
+		# Compute gradient
+		self.gradtheta = T.grad(self.cost, self.theta)
+		# Build objective function
+		# Compute the gradients
+		self.compute_cost_and_gradient = theano.function(inputs=[self.input, self.truth], 
+												outputs=[self.cost, self.gradtheta])
+		# Build prediction function
+		self.predict = theano.function(inputs=[self.input], outputs=self.pred)
+		if verbose:
+			pprint('*' * 50)
+			pprint('Finished constructing Bidirectional Recurrent Neural Network (BRNN)')
+			pprint('Size of input dimension: %d' % configs.num_input)
+			pprint('Size of hidden/recurrent dimension: %d' % configs.num_hidden)
+			pprint('Size of output dimension: %d' % configs.num_class)
+			pprint('Is regularization applied? %s' % ('yes' if configs.regularization else 'no'))
+			if configs.regularization:
+				pprint('Coefficient of regularization term: %f' % configs.lambda1)
+			pprint('Number of free parameters in TBRNN: %d' % self.num_params)
+			pprint('*' * 50)
+
+	def train(self, input, truth, learn_rate):
+		cost = self.objective(input, truth, learn_rate)
+		return cost
+
+	# This method is used to implement the batch updating algorithm
+	def update_params(self, gradtheta, learn_rate):
+		# gradparams is a single long vector which can be used to update self.theta
+		# Learning algorithm: simple stochastic gradient descent
+		theta = self.theta.get_value(borrow=True)
+		self.theta.set_value(theta - learn_rate * gradtheta, borrow=True)
+
+	@staticmethod
+	def save(fname, model):
+		with file(fname, 'wb') as fout:
+			cPickle.dump(model, fout)
+
+	@staticmethod
+	def load(fname):
+		with file(fname, 'rb') as fin:
+			return cPickle.load(fin)
+
