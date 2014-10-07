bfc9e8f415cc3e5b64e86ae2019ecf8d68706dca
HanZhao <zhaohan2009011312@gmail.com>
2014-10-07 14:30:26 +0800

    first version of gating recursive convolutional neural network

--- source/grcnn.py
+++ source/grcnn.py
@@ -31,7 +31,6 @@ class GRCNNEncoder(object):
         if verbose: logger.debug('Building Gated Recursive Convolutional Neural Network Encoder...')
         # Make theano symbolic tensor for input and model parameters
         self.input = T.matrix(name='input', dtype=floatX)
-        self.learn_rate = T.scalar(name='learning rate')
         # Configure activation function
         self.act = Activation(config.activation)
         fan_in, fan_out = config.num_input, config.num_hidden
@@ -44,6 +43,7 @@ class GRCNNEncoder(object):
                                       high=np.sqrt(6.0/(fan_in+fan_out)),
                                       size=(fan_in, fan_out)), dtype=floatX),
                     name='U', borrow=True)
+        self.hidden0 = T.dot(self.input, self.U)
         # W^l, W^r, parameters used to construct the central hidden representation
         self.Wl = theano.shared(value=np.asarray(
                     np.random.uniform(low=-np.sqrt(6.0/(fan_out+fan_out)),
@@ -70,26 +70,40 @@ class GRCNNEncoder(object):
         self.Gb = theano.shared(value=np.zeros(3, dtype=floatX), name='Gb', borrow=True)
         # Save all the parameters into one batch
         self.params = [self.U, self.Wl, self.Wr, self.Wb, self.Gl, self.Gr, self.Gb]
+        # Length of the time sequence
+        nsteps = self.input.shape[0]
+        # mask = T.alloc(1.0, nsteps, 1)
+        self.pyramids, _ = theano.scan(fn=self._step_prop, 
+                                        outputs_info=[self.hidden0],
+                                        n_steps=nsteps-1)
+        self.output = self.pyramids[-1][0].dimshuffle('x', 0)
+        # Compression -- Encoding function
+        self.compress = theano.function(inputs=[self.input], outputs=self.output)
         if verbose:
             logger.debug('Finished constructing the structure of grCNN Encoder: ')
             logger.debug('Size of the input dimension: %d' % fan_in)
             logger.debug('Size of the hidden dimension: %d' % fan_out)
             logger.debug('Activation function: %s' % config.activation)
 
-
     def _step_prop(self, current_level):
         '''
         @current_level: Input matrix at current level. The first dimension corresponds to 
         the timestamp while the second dimension corresponds to the dimension of hidden representation
         '''
-        # Build shifted matrix, of size (T-1)xd
-        right_current_level = current_level[1:]
-        left_current_level = current_level[:-1]
-        # Compute temporary central hidden representation, of size (T-1)xd
-        central_current_level = self.act(T.dot(left_current_level, self.Wl) + 
-                                         T.dot(right_current_level, self.Wr) + 
-                                         self.Wb)
-        # Compute gating function, of size (T-1)x3
+        # Build shifted matrix, due to the constraints of Theano.scan, we have to keep the shape of the
+        # input and output matrix
+        right_current_level = T.zeros_like(current_level)
+        right_current_level = T.set_subtensor(right_current_level[:-1], current_level[1:])
+        left_current_level = T.zeros_like(current_level)
+        left_current_level = T.set_subtensor(left_current_level[:-1], current_level[:-1])
+        # Compute temporary central hidden representation, of size Txd, but we only care about the first
+        # T-1 rows, i.e., we only focus on the (T-1)xd sub-matrix.
+        central_current_level = self.act.activate(T.dot(left_current_level, self.Wl) + 
+                                                  T.dot(right_current_level, self.Wr) + 
+                                                  self.Wb)
+        # Compute gating function, of size Tx3. Again, due to the internal limitation of Theano.scan, we cannot
+        # reduce the size of the matrix and have to keep the same size, but actually we only want the first (T-1)x3
+        # sub-matrix.
         current_gates = T.nnet.softmax(T.dot(left_current_level, self.Gl) + 
                                        T.dot(right_current_level, self.Gr) + 
                                        self.Gb)
@@ -105,14 +119,34 @@ class GRCNNEncoder(object):
                      central_gate * central_current_level
         return next_level
 
-
-
-
-
-
-
-
-
-
-
-
+    def _step_prop_reduce(self, current_level):
+        '''
+        @current_level: Input matrix at current level. The first dimension corresponds to 
+        the timestamp while the second dimension corresponds to the dimension of hidden representation
+        '''
+        # Build shifted matrix, due to the constraints of Theano.scan, we have to keep the shape of the
+        # input and output matrix
+        right_current_level = current_level[1:]
+        left_current_level = current_level[:-1]
+        # Compute temporary central hidden representation, of size Txd, but we only care about the first
+        # T-1 rows, i.e., we only focus on the (T-1)xd sub-matrix.
+        central_current_level = self.act.activate(T.dot(left_current_level, self.Wl) + 
+                                                  T.dot(right_current_level, self.Wr) + 
+                                                  self.Wb)
+        # Compute gating function, of size Tx3. Again, due to the internal limitation of Theano.scan, we cannot
+        # reduce the size of the matrix and have to keep the same size, but actually we only want the first (T-1)x3
+        # sub-matrix.
+        current_gates = T.nnet.softmax(T.dot(left_current_level, self.Gl) + 
+                                       T.dot(right_current_level, self.Gr) + 
+                                       self.Gb)
+        left_gate, central_gate, right_gate = current_gates[:, 0], current_gates[:, 1], current_gates[:, 2]
+        # Reshape for broadcasting
+        left_gate = left_gate.dimshuffle(0, 'x')
+        central_gate = central_gate.dimshuffle(0, 'x')
+        right_gate = right_gate.dimshuffle(0, 'x')
+        # Build next level of hidden representation using soft combination,
+        # matrix of size (T-1)xd
+        next_level = left_gate * left_current_level + \
+                     right_gate * right_current_level + \
+                     central_gate * central_current_level
+        return next_level
--- test/testgrCNN.py
+++ test/testgrCNN.py
@@ -9,8 +9,15 @@ import os, sys
 import unittest
 import theano
 import theano.tensor as T
+import numpy as np
 import logging
+import pprint
+import time
 
+# Set the basic configuration of the logging system
+logging.basicConfig(level=logging.DEBUG,
+                    format='%(asctime)s %(name)-12s %(levelname)-8s %(message)s',
+                    datefmt='%m-%d %H:%M')
 sys.path.append('../source/')
 logger = logging.getLogger(__name__)
 
@@ -22,10 +29,92 @@ class TestGRCNN(unittest.TestCase):
         fname = './grCNN.conf'
         self.configer = GRCNNConfiger(fname)
 
+    @unittest.skip('Finished building, no problem')
     def testBuilding(self):
+        logger.debug('Inside testBuilding...')
         grcnn = GRCNNEncoder(self.configer, verbose=True)
 
+    @unittest.skip('One Step propagation function passed.')
+    def testOneStep(self):
+        logger.debug('Inside testOneStep...')
+        grcnn = GRCNNEncoder(self.configer, verbose=True)
+        time_steps = 30000
+        input_matrix = np.random.rand(time_steps, 10)
+        input_matrix = input_matrix.astype(np.float32)
+        logger.debug('Input Matrix Shape: {}'.format(input_matrix.shape))
+        # Manually compute the result
+        U = grcnn.U.get_value(borrow=True)
+        Wl = grcnn.Wl.get_value(borrow=True)
+        Wr = grcnn.Wr.get_value(borrow=True)
+        Wb = grcnn.Wb.get_value(borrow=True)
+        Gl = grcnn.Gl.get_value(borrow=True)
+        Gr = grcnn.Gr.get_value(borrow=True)
+        Gb = grcnn.Gb.get_value(borrow=True)
+        # Build function
+        input_symbol = T.matrix()
+        hidden_symbol = T.dot(input_symbol, U)
+        output_symbol = grcnn._step_prop(hidden_symbol)
+        f = theano.function(inputs=[input_symbol], outputs=output_symbol)
+        # Start testing
+        start_time = time.time()
+        output_matrix = f(input_matrix)
+        end_time = time.time()
+        logger.debug('Time used by Theano: %f seconds.' % (end_time-start_time))
+        def softmax(x):
+            y = np.exp(x)
+            return y / np.sum(y, axis=1)[:, np.newaxis]
+        start_time = time.time()
+        hidden_matrix = np.dot(input_matrix, U)
+        left_T, right_T = hidden_matrix[:-1], hidden_matrix[1:]
+        central_T = np.tanh(np.dot(left_T, Wl) + np.dot(right_T, Wr) + Wb)
+        gates = softmax(np.dot(left_T, Gl) + np.dot(right_T, Gr) + Gb)
+        left_gate, central_gate, right_gate = gates[:, 0], gates[:, 1], gates[:, 2]
+        next_level = left_T * left_gate[:, np.newaxis] + \
+                     right_T * right_gate[:, np.newaxis] + \
+                     central_T * central_gate[:, np.newaxis]
+        end_time = time.time()
+        logger.debug('Time used by Numpy: %f seconds.' % (end_time-start_time))
+        diff1 = np.sum(np.abs(next_level-output_matrix[:-1]))
+        logger.debug('L1 difference: {}'.format(diff1))
+        diff2 = np.sum((next_level-output_matrix[:-1]) ** 2)
+        logger.debug('L2 difference: {}'.format(diff2))
 
+    # @unittest.skip('Wait a minute')
+    def testCompress(self):
+        logger.debug('Inside testCompress...')
+        grcnn = GRCNNEncoder(self.configer, verbose=True)
+        # Build step transformation function 
+        input_symbol = T.matrix()
+        output_symbol = grcnn._step_prop_reduce(input_symbol)
+        f = theano.function(inputs=[input_symbol], outputs=output_symbol)
+        time_steps = 60
+        input_matrix = np.random.rand(time_steps, self.configer.num_input)
+        input_matrix = input_matrix.astype(np.float32)
+        # Timing for numpy iterative application
+        start_time = time.time()
+        U = grcnn.U.get_value(borrow=True)
+        hidden_matrix = np.dot(input_matrix, U)
+        logger.debug('Input Matrix Shape: {}'.format(hidden_matrix.shape))
+        for i in xrange(time_steps-1):
+            hidden_matrix = f(hidden_matrix)
+        end_time = time.time()
+        logger.debug('Time used for manually iterative application: {} seconds.'.format(end_time-start_time))
+        # logger.debug('Output Matrix by manually iterative application: ')
+        # logger.debug(hidden_matrix)
+        logger.debug('Output Matrix Shape: {}'.format(hidden_matrix.shape))
+        logger.debug('=' * 50)
+        start_time = time.time()
+        output_matrix = grcnn.compress(input_matrix)
+        end_time = time.time()
+        logger.debug('Time used for manually iterative application: {} seconds.'.format(end_time-start_time))
+        logger.debug('Output Matrix Shape: {}'.format(output_matrix.shape))
+        # logger.debug('Output Matrix by theano.scan compress function: ')
+        # logger.debug(output_matrix)
+        diff1 = np.sum(np.abs(hidden_matrix-output_matrix))
+        diff2 = np.sum((hidden_matrix-output_matrix) ** 2)
+        logger.debug('*' * 50)
+        logger.debug('L1 difference between manually application and Theano compression: {}'.format(diff1))
+        logger.debug('L2 difference between manually application and Theano compression: {}'.format(diff2))
 
 if __name__ == '__main__':
     unittest.main()
