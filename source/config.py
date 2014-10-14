3b91270fa436180e3d68dafd243e8641cac04a16
HanZhao <zhaohan2009011312@gmail.com>
2014-10-14 09:59:16 +0800

    Using dropout technique as a default for models

--- source/config.py
+++ source/config.py
@@ -19,7 +19,8 @@ class CNNConfiger(object):
 		self._cf_parser = ConfigParser.ConfigParser()
 		self._cf_parser.read(fname)
 		# Parsing 
-		self.activation, self.nepoch, self.batch_size, self.image_row, self.image_col, \
+		self.activation, self.nepoch, self.dropout, self.random_seed, self.batch_size, \
+		self.image_row, self.image_col, \
 		self.num_convpool, self.num_hidden, self.num_softmax, self.convs, self.pools, self.hiddens, \
 		self.softmaxs = self.parse()
 
@@ -33,6 +34,8 @@ class CNNConfiger(object):
 	def parse(self):
 		activation = self._cf_parser.get('functions', 'activations')
 		nepoch = self._cf_parser.getint('parameters', 'nepoch')
+		dropout = self._cf_parser.getfloat('parameters', 'dropout')
+		random_seed = self._cf_parser.getfloat('parameters', 'random_seed')
 		batch_size = self._cf_parser.getint('input', 'batchsize')
 		image_row = self._cf_parser.getint('input', 'imagerow')
 		image_col = self._cf_parser.getint('input', 'imagecol')
@@ -62,7 +65,7 @@ class CNNConfiger(object):
 			l = [int(x) for x in l.split(',')]
 			softmaxs.append(l)
 
-		return (activation, nepoch, batch_size, image_row, image_col, 
+		return (activation, nepoch, dropout, random_seed, batch_size, image_row, image_col, 
 				num_convpool, num_hidden, num_softmax, convs, pools, hiddens, softmaxs)
 
 
@@ -77,7 +80,8 @@ class MLPConfiger(object):
 		self._cf_parser = ConfigParser.ConfigParser()
 		self._cf_parser.read(fname)
 		# Parsing 
-		self.activation, self.nepoch, self.batch_size, self.sparsity, \
+		self.activation, self.nepoch, self.dropout, self.random_seed,\
+		self.batch_size, self.sparsity, \
 		self.lambda1, self.lambda2, self.num_hidden, self.num_softmax, \
 		self.hiddens, self.softmaxs = self.parse()
 
@@ -91,6 +95,8 @@ class MLPConfiger(object):
 	def parse(self):
 		activation = self._cf_parser.get('functions', 'activations')
 		nepoch = self._cf_parser.getint('parameters', 'nepoch')
+		dropout = self._cf_parser.getfloat('parameters', 'dropout')
+		random_seed = self._cf_parser.getint('parameters', 'random_seed')
 		batch_size = self._cf_parser.getint('input', 'batchsize')
 		num_hidden = self._cf_parser.getint('architectures', 'hidden')
 		num_softmax = self._cf_parser.getint('architectures', 'softmax')
@@ -111,7 +117,7 @@ class MLPConfiger(object):
 			l = [int(x) for x in l.split(',')]
 			softmaxs.append(l)
 
-		return (activation, nepoch, batch_size, sparsity,
+		return (activation, nepoch, dropout, random_seed, batch_size, sparsity,
 				lambda1, lambda2, num_hidden, num_softmax, hiddens, softmaxs)
 
 
@@ -126,7 +132,8 @@ class DAEConfiger(object):
 		self._cf_parser = ConfigParser.ConfigParser()
 		self._cf_parser.read(fname)
 		# Parsing 
-		self.activation, self.nepoch, self.seed, self.denoising, self.sparsity, \
+		self.activation, self.nepoch, self.dropout, self.random_seed, \
+		self.denoising, self.sparsity, \
 		self.lambda1, self.mask, self.num_hidden, self.hiddens = self.parse()
 
 	def get(self, cfg_object, cfg_section):
@@ -141,11 +148,12 @@ class DAEConfiger(object):
 		nepoch = self._cf_parser.getint('parameters', 'nepoch')
 		lambda1 = self._cf_parser.getfloat('parameters', 'lambda1')
 		mask = self._cf_parser.getfloat('parameters', 'mask')
+		dropout = self._cf_parser.getfloat('parameters', 'dropout')
+		random_seed = self._cf_parser.getint('parameters', 'random_seed')
 		num_hidden = self._cf_parser.getint('architectures', 'hidden')
 		
 		sparsity = self._cf_parser.getint('parameters', 'sparsity')
 		denoising = self._cf_parser.getint('parameters', 'denoising')
-		seed = self._cf_parser.getint('parameters', 'seed')
 		# Load architecture of convolution and pooling layers
 		hiddens = []
 		# Load detailed architecture for each layer
@@ -154,7 +162,7 @@ class DAEConfiger(object):
 			l = [int(x) for x in l.split(',')]
 			hiddens.append(l)
 
-		return (activation, nepoch, seed, denoising, sparsity,
+		return (activation, nepoch, dropout, random_seed, denoising, sparsity,
 				lambda1, mask, num_hidden, hiddens)
 
 
