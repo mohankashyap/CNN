df4de9b87aab7410bc3ac74f3fdfc6df3cb3c507
HanZhao <zhaohan2009011312@gmail.com>
2014-10-29 11:35:07 +0800

    test on the hard task

--- experiment/exp_grcnn_ranking.py
+++ experiment/exp_grcnn_ranking.py
@@ -243,6 +243,7 @@ try:
             score_p, score_n = score_p[0], score_n[0]
             if score_p < 1+score_n: costs += 1-score_p+score_n
             preds.append(score_p >= score_n)
+            logger.debug('Instance %d, score-p: %f, score-n: %f' % (j, score_p, score_n))
         return costs, preds
 
     # for i in xrange(configer.nepoch):
