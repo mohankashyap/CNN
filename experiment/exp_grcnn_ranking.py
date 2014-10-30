f5ce3c73f732eb89776c2630ff3e1dcdcf7f1e2e
Han-Noah <zhaohan2009011312@gmail.com>
2014-10-30 21:19:51 +0800

    save

--- experiment/exp_grcnn_ranking.py
+++ experiment/exp_grcnn_ranking.py
@@ -130,8 +130,8 @@ logger.debug('Time used to build training and test data set: %f seconds.' % (end
 config_filename = './grCNN_ranker.conf'
 start_time = time.time()
 configer = GrCNNConfiger(config_filename)
-# grcnn = GrCNNMatchScorer(configer, verbose=True)
-grcnn = GrCNNMatchScorer.load('./GrCNNMatchRanker-RANK-GOOD.pkl')
+grcnn = GrCNNMatchScorer(configer, verbose=True)
+#grcnn = GrCNNMatchScorer.load('./GrCNNMatchRanker-RANK-GOOD.pkl')
 end_time = time.time()
 logger.debug('Time used to build/load GrCNNMatchRanker: %f seconds.' % (end_time-start_time))
 # Define negative/positive sampling ratio
